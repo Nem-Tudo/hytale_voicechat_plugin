450684b9642e0aabd639046902e043effd859acd
Nem Tudo <gf98289@gmail.com>
2026-01-20 19:41:18 -0300

    feat: dev command

--- src/main/java/me/nemtudo/voicechat/VoiceChatCommandDev.java
+++ src/main/java/me/nemtudo/voicechat/VoiceChatCommandDev.java
@@ -1,8 +1,8 @@
 package me.nemtudo.voicechat;
 
+import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.command.system.AbstractCommand;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
-import com.hypixel.hytale.server.core.Message;
 import me.nemtudo.voicechat.utils.GenerateCode;
 
 import javax.annotation.Nonnull;
@@ -10,17 +10,16 @@ import javax.annotation.Nullable;
 import java.awt.*;
 import java.util.concurrent.CompletableFuture;
 
-public class VoiceChatCommand extends AbstractCommand {
+public class VoiceChatCommandDev extends AbstractCommand {
 
     private final VoiceChat plugin;
 
     // Opção 1: Recebe o plugin no construtor (RECOMENDADO)
-    public VoiceChatCommand(VoiceChat plugin) {
-        super("voicechat", "Get the voice chat link", false);
+    public VoiceChatCommandDev(VoiceChat plugin) {
+        super("dev", "Get the DEV voice chat link", false);
         this.plugin = plugin;
         this.addSubCommand(new VoiceChatCommandReload(this.plugin));
         this.addSubCommand(new VoiceChatCommandDownload(this.plugin));
-        this.addSubCommand(new VoiceChatCommandDev(this.plugin));
     }
 
     @Override
@@ -39,11 +38,12 @@ public class VoiceChatCommand extends AbstractCommand {
 
         // Envia a mensagem com o link
 
-        String finalLink = plugin.config.get().getBaseUrl() + "/" + serverId + "/" + playerCode;
+        String finalLink = plugin.config.get().getBaseUrl() + "/dev/" + serverId + "/" + playerCode;
 
         context.sender().sendMessage(Message.raw("Link (click): " + finalLink).link(finalLink).color(Color.green));
         context.sender().sendMessage(Message.raw("SERVER ID: " + serverId));
         context.sender().sendMessage(Message.raw("YOUR CODE: " + playerCode));
+        context.sender().sendMessage(Message.raw("DEV VERSION! EXPECT BUGS").color(Color.orange));
 
         return CompletableFuture.completedFuture(null);
     }
